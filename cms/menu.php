b406a04c28b160983c9ecbff8ada66c74f02b162
Sérgio Monteiro Jr. <sergio@publixcomunicacao.com.br>
2014-12-17 13:28:59 -0200

    Mudança Menu

--- cms/menu.php
+++ cms/menu.php
@@ -12,53 +12,7 @@
             <a href="usuario_cadastra.php">Novo Usu&aacute;rios </a>
             <a href="usuario_gerencia.php">Gerenciamento de Usu&aacute;rios</a>
         </div>
-        
-        <a href="#">Institucional</a>
-        <div>
-        	<a href="institucional_cadastra2.php?alterar=1">Gerenciamento de Texto Institucional</a>
-        </div> 
-        
-       <a href="#">Parceiros</a>
-       <div>
-       		<a href="parceiros_cadastra.php">Gerenciamento de Parceiros</a>
-       </div> 
-       
-       <a href="#">Notícias</a>
-       <div>
-       		<a href="noticia_cadastra.php">Gerenciamento de Notícias</a>
-       </div>
-       
-        <a href="#">O Aikido</a>
-       <div>
-       		<a href="institucional_cadastra.php?alterar=1">Gerenciamento do Texto aikido</a>
-       		<a href="perfil_cadastra.php">Gerenciamento de Perfil</a>
 
-       </div>
-       
-       <a href="#">Estrutura</a>
-       <div>
-       		<a href="estrutura_cadastra.php?alterar=1">Gerenciamento do Texto de Estrutura</a>
-       		<a href="instrutores_cadastra.php">Gerenciamento de Instrutores</a>
-       		<a href="estrutura_cadastra_fotos.php">Gerenciamento de Fotos</a>
-       		<a href="faixas_cadastra.php">Gerenciamento de Faixas</a>
-       </div>
-       
-       <a href="#">Projetos Sociais</a>
-       <div>
-       		<a href="social_cadastra.php?alterar=1">Gerenciamento de Projetos Sociais</a>
-       		<a href="social_cadastra_fotos.php">Gerenciamento de Fotos</a>
-       </div>
-       
-       <a href="#">Eventos</a>
-       <div>
-       		<a href="eventos_cadastra.php">Gerenciamento de Eventos</a>
-       </div>
-       
-        <a href="#">Contato</a>
-       <div>
-       		<a href="contato_cadastra.php?alterar=1">Gerenciamento do Texto de Contato</a>
-       		<a href="contato_email_cadastra.php">Gerenciamento de Contatos</a>
-       </div>
         
    <!--       
         <a href="#">A Home</a>
